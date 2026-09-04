9fb0fb9b831352333d80b15b0bfdfe65ed7f9241
hayootaeltoota777-blip <hayootaeltoota777@gmail.com>
2026-09-05 00:50:12 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -23,7 +23,7 @@ export const botConfig = {
     // 5 = Competing
     activities: [
       {
-        name: "Custom Status", // required by Discord API, not shown in the client
+        name: "يتزحلك في براول", // required by Discord API, not shown in the client
         state: "يتزحلك في براول",     // this is what people actually see
         type: 1,               // Custom
       },
